f9932915c2efd770292143ddb064f663b4647d81
김솔 <rlathfrlathf@gmail.com>
2022-03-31 18:17:44 +0900

    이거로 받아봐

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.teamproject;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,21 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 데이터 원본 준비
-        String[] items = {"item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8"};
-
-
-        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
-        ArrayAdapter<String> adapt
-                = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                items);
-
-        // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        // 어댑터를 GridView 객체에 연결
-        gridview.setAdapter(adapt);
 
     }
 }
