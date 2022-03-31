2a32f51c54a1e5ab9d39edbdba3dec2f765f5f62
김솔 <rlathfrlathf@gmail.com>
2022-03-31 18:54:40 +0900

    대ㅗㅅ다

--- app/src/main/java/com/example/teamproject/MainActivity.java
+++ app/src/main/java/com/example/teamproject/MainActivity.java
@@ -13,6 +13,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // 데이터 원본 준비
+        String[] items = new String[42];
+        for(int i=0;i<42;i++)
+        {
+            items[i] = "1";
+        }
+
+        //어댑터 준비 (배열 객체 이용, simple_list_item_1 리소스 사용
+        ArrayAdapter<String> adapt
+                = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                items);
+
+        // id를 바탕으로 화면 레이아웃에 정의된 GridView 객체 로딩
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        // 어댑터를 GridView 객체에 연결
+        gridview.setAdapter(adapt);
 
     }
 }
